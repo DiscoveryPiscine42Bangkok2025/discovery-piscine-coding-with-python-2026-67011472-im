40d68ede5901d86a4fa533b0d7bbeb985730b019
67011472-im <67011472@kmitl.ac.th>
2026-02-11 03:50:51 +0700

    Add files via upload

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,1 +1,9 @@
+print("Enter a number less than 25")
+user_input = int(input())
 
+if user_input > 25:
+    print("Error")
+else:
+    while user_input <= 25:
+        print("Inside the loop, my variable is", user_input)
+        user_input += 1  # เพิ่มค่าทีละ 1 ในแต่ละรอบ
