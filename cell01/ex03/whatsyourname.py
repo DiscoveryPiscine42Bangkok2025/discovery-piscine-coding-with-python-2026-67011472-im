5a2b4346526d3b861521373ff4fd2603aac401dc
67011472-im <67011472@kmitl.ac.th>
2026-02-11 02:10:35 +0700

    Add files via upload

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,1 +1,5 @@
+first_name = input("Hey, what's your first name? : ").strip()
+last_name = input("And your last name? : ").strip()
 
+print("Well, pleased to meet you, " + first_name + " " + last_name + ".")
+# .strip() ตัดช่องว่างหน้าหลัง
