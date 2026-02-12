62727550965aa13a45145a495398510583797777
67011472-im <67011472@kmitl.ac.th>
2026-02-12 08:19:58 +0700

    Add files via upload

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,12 @@
+import sys
+
+argn = sys.argv[1:]
+if len(argn) != 1:
+    print("none")
+else:
+    text = argn[0]
+    zchar = [ch for ch in text if ch == "z"]
+    if zchar:
+        print("".join(zchar))
+    else:
+        print("none")
